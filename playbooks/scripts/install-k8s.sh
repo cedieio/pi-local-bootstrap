3664f5a416f8a5efd194c884be0e139e27dc5f16
Cedric Villanueva <cedric.villanueva@nine.com.au>
2024-09-09 12:05:42 +1000

    🔧 Add base configs
    Base configs for raspberry-pi bootstrap via ansible.

---
+++ playbooks/scripts/install-k8s.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Install Linux Modules for Raspberry Pi, disable swap and add kernel settings
+apt-get install linux-modules-extra-raspi
+
+swapoff -a
+sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+modprobe overlay
+modprobe br_netfilter
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+sysctl --system
+
+# Install Containerd
+
+apt-get update -y
+apt-get install ca-certificates curl gnupg lsb-release -y
+
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update -y
+apt-get install containerd.io -y
+containerd config default > /etc/containerd/config.toml
+
+sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
+
+systemctl restart containerd
+systemctl enable containerd
+
+# Install kubeadm, kubelet and kubectl 
+apt-get update
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+apt-get update
+apt-get install -y kubelet kubeadm kubectl
+apt-mark hold kubelet kubeadm kubectl
